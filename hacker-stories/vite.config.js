3d212510ab18e39eb8b5b5c5f9b22ba341def393
mmadero016 <131493783+mmadero016@users.noreply.github.com>
2025-05-04 23:07:53 -0400

    f

--- hacker-stories/vite.config.js
+++ hacker-stories/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ist363/hacker-stories/',  // 👈 add this line
   plugins: [react()],
 })
